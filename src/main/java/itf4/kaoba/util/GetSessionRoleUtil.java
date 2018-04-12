0439fa0a0643c46f8f5ea8354cd65395bbf988fb
jhone <jhonexiaomei@gmail.com>
2018-04-12 16:10:20 +0800

    GetSessionRoleUtil 优化

--- src/main/java/itf4/kaoba/util/GetSessionRoleUtil.java
+++ src/main/java/itf4/kaoba/util/GetSessionRoleUtil.java
@@ -19,6 +19,7 @@ import itf4.kaoba.pojo.RolePojo;
 public class GetSessionRoleUtil {
 	private static RolePojo rolePojo = new RolePojo();
 	private static Class<?> clazz;
+	private static Object object;
 	private static String attribute = null;
 	private static Method method =null;
 	private static String preffix = "get";
@@ -27,52 +28,51 @@ public class GetSessionRoleUtil {
 	public static RolePojo getRole(HttpSession session) throws Exception {
 		
 		String CurrUserRoleFlag =(String)session.getAttribute("CurrUserRoleFlag");
-		Object object = session.getAttribute("CurrentLoginUserInfo");
+		object = session.getAttribute("CurrentLoginUserInfo");
 		
 		if(object !=null) {
-			Class<?> clazz = object.getClass();
+			clazz = object.getClass();
 			//管理员
 			if(CurrUserRoleFlag.equals("1")) {
 			    //姓名
-				rolePojo.setLoginName(getAttribute("UserName",object));
+				rolePojo.setLoginName(getAttribute("UserName"));
 				//密码
-				rolePojo.setLoginPassword(getAttribute("UserPassword",object));
+				rolePojo.setLoginPassword(getAttribute("UserPassword"));
 			
 			//老师
 			} else if(CurrUserRoleFlag.equals("2")) {
 				//姓名
-				rolePojo.setLoginName(getAttribute("TeaName",object));
+				rolePojo.setLoginName(getAttribute("TeaName"));
 				//密码
-				rolePojo.setLoginPassword(getAttribute("TeaPassword",object));
+				rolePojo.setLoginPassword(getAttribute("TeaPassword"));
 		    
 				//学生
 			} else if(CurrUserRoleFlag.equals("3")) {
 				//姓名
-				rolePojo.setLoginName(getAttribute("StuName",object));
+				rolePojo.setLoginName(getAttribute("StuName"));
 				//密码
-				rolePojo.setLoginPassword(getAttribute("StuPassword",object));
+				rolePojo.setLoginPassword(getAttribute("StuPassword"));
 			}
 		}
 		
 		//主键
-		rolePojo.setId(Integer.parseInt(getAttribute("Id",object)));
+		rolePojo.setId(Integer.parseInt(getAttribute("Id")));
 		//标志
-		rolePojo.setStatus(Integer.parseInt(getAttribute("Status",object)));
+		rolePojo.setStatus(Integer.parseInt(getAttribute("Status")));
 		//创建者
-		rolePojo.setCreater(getAttribute("Creater",object));
+		rolePojo.setCreater(getAttribute("Creater"));
 		//创建时间
-		rolePojo.setCreateTime(getAttribute("CreateTime",object));
+		rolePojo.setCreateTime(getAttribute("CreateTime"));
 		//更新者
-		rolePojo.setUpdater(getAttribute("Updater",object));
+		rolePojo.setUpdater(getAttribute("Updater"));
 		//更新时间
-		rolePojo.setUpdateTime(getAttribute("UpdateTime",object));
+		rolePojo.setUpdateTime(getAttribute("UpdateTime"));
 		
 		return rolePojo;
 	}
 	
 	//根据属性名称，获取属性值
-	private static String getAttribute(String name,Object object) throws Exception{
-		clazz = object.getClass();
+	private static String getAttribute(String name) throws Exception{
 		method = clazz.getMethod(preffix +name, null);
 		attribute = String.valueOf(method.invoke(object, null));
 		return attribute;
